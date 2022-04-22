addce58b27ed020bf3d9d2634de2d81b18656598
mohamed_sarrou <mohamedserrou2016@gmail.com>
2022-04-22 11:48:44 +0000

    initialisation

---
+++ TENNIS_APP/Models/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TENNIS_APP.Models;
+
+namespace TENNIS_APP.Models
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Joueur> joueurs { get; set; }
+        public DbSet<Rencontre> rencontres { get; set; }
+        public DbSet<Gain> gains { get; set; }
+        public DbSet<Sponsor> sponsors { get; set; }
+        
+        public DbSet<Tournoi> Tournois { get; set; }
+        
+        public DbSet<TENNIS_APP.Models.Subvention> Subvention { get; set; }
+    }
+}
